b60948f7e0831ee695618f3cb59ef13496a4c388
Miguel <mccantuta@Miguels-MacBook-Pro.local>
2019-10-31 18:45:23 -0500

    Initial commit

---
+++ src/test/java/com/mccl/poc/springbatchpoc/SpringbatchPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccl.poc.springbatchpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
